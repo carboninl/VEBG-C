b4742abfa8ab18399e2a0a3d702add48af16195b
JustinDiaz001 <justindiaz851@gmail.com>
2026-06-04 22:42:27 -0700

    WIP: start given desugaring support

--- ast.h
+++ ast.h
@@ -1,3 +1,10 @@
+#ifndef AST_H
+#define AST_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 typedef enum {
     NUM_C,
     BOOL_C,
@@ -40,3 +47,68 @@ struct ExprC {
     ExprC **args;
     int num_args;
 };
+
+static ExprC *new_num(double n) {
+    ExprC *expr = malloc(sizeof(ExprC));
+    if (expr == NULL) {
+        perror("malloc");
+        exit(1);
+    }
+
+    expr->type = NUM_C;
+    expr->num = n;
+    return expr;
+}
+
+static ExprC *new_bool(int b) {
+    ExprC *expr = malloc(sizeof(ExprC));
+    if (expr == NULL) {
+        perror("malloc");
+        exit(1);
+    }
+
+    expr->type = BOOL_C;
+    expr->boolean = b;
+    return expr;
+}
+
+static ExprC *new_str(char *s) {
+    ExprC *expr =malloc(sizeof(ExprC));
+    if (expr == NULL) {
+        perror("malloc");
+        exit(1);
+    }
+
+    expr->type = STR_C;
+    expr->str = s;
+    return expr;
+}
+
+static ExprC *new_if(ExprC *cond, ExprC *then_branch, ExprC *else_branch) {
+    ExprC *expr = malloc(sizeof(ExprC));
+    if (expr == NULL) {
+        perror("malloc");
+        exit(1);
+    }
+
+    expr->type = IF_C;
+    expr->cond = cond;
+    expr->then_branch = then_branch;
+    expr->else_branch = else_branch;
+    return expr;
+}
+
+static ExprC *new_lam(char **params, int num_params, ExprC *body) {
+    ExprC *expr = malloc(sizeof(ExprC)){
+        if (expr == NULL) {
+            perror("malloc");
+            exit(1);
+        }
+    }
+
+    expr->type = LAM_C;
+    expr->params = params;
+    expr->num_params = num_params;
+    expr->body = body;
+    return expr;
+}
